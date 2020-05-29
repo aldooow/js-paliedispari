8f48645fbba5b7591fba57e09e6039f51096a004
Aldo Cahuana <aldooow@icloud.com>
2020-05-29 17:59:21 +0200

    Work in progress!!!

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
- // Esercizio 1
+//  // Esercizio 1
 function checkPalindroma(str) {
     return str == str.split('').reverse().join('');
 }
@@ -13,10 +13,30 @@ if (rilsultato == true) {
   console.log('Questa, non è una parola Palindroma')
 }
 
- // Esercizio 2
+// Esercizio 2
 
- // Parola da Ramdom da inserire
 
- // Sceglie Pari o Dispari
+// Sceglie Pari o Dispari!
+var scegliePariDispari = prompt('sceglie Pari o Dispari')
 
- function numeroRandom
+// Sceglie un numero del 1 al 5!!!
+var numeroUtente = parseInt(prompt('Sceglie un numero del 1 al 5'));
+
+// Genera un numero random del 1 al 5!!!
+ var numeroRandom = parseInt(Math.floor(Math.random() * 5) + 1);
+ console.log(numeroRandom)
+
+// Sommare tutti 2 numeri, e dichiarare se è pari o dispare,
+function checkSommaPariDispari() {
+  var risultato;
+  if ((numeroUtente + numeroRandom) % 2 == 0) {
+    alert('La somma è pari')
+    risultato = 'pari'
+  } else (
+    alert('La somma è dispari')
+
+  )
+  return risultato;
+}
+var risultato = checkSommaPariDispari()
+// e coincide con la tu risposta iniziale, hai vinto!!!
