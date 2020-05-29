e1621b1c17b1c32e86aa373809e63ea5991005f0
Aldo Cahuana <aldooow@icloud.com>
2020-05-29 17:05:36 +0200

    Work in progress!!!

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+ // Esercizio 1
+function checkPalindroma(str) {
+    return str == str.split('').reverse().join('');
+}
+
+var rilsultato = checkPalindroma('aalsa');
+console.log(rilsultato)
+
+if (rilsultato == true) {
+  console.log('SI è una parola Palindroma')
+} else {
+  console.log('Questa, non è una parola Palindroma')
+}
+
+ // Esercizio 2
+
+ // Parola da Ramdom da inserire
+
+ // Sceglie Pari o Dispari
+
+ function numeroRandom
